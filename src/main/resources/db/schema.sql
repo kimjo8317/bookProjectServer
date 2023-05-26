2ca8cc2e7d3f024b5983354113d4141a9fdc2ccb
kimjunoh <kimjo8317@gmail.com>
2023-05-26 16:01:27 +0900

    mysql schema 작성

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,46 +1,75 @@
 -- ================ bookinfo 책정보
-DROP TABLE IF EXISTS bookinfo;
-CREATE TABLE bookinfo(
-    bookinfo varchar(100) NOT NULL PRIMARY KEY, -- 책 제목
-    writer varchar(100) NOT NULL, -- 지은이
-    poster varchar NOT NULL, -- 표지 사진
-    pub varchar(100) NOT NULL, -- 출판사
-    summary varchar(5000) NOT NULL, -- 줄거리
-    gen varchar(50) NOT NULL -- 장르
+DROP TABLE IF EXISTS 책정보;
+CREATE TABLE 책정보(
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(200) NOT NULL ,
+    writer varchar(100) NOT NULL,
+    genre varchar(100) NOT NULL,
+    publisher varchar(100) NOT NULL,
+    publication_date DATE NOT NULL,
+    poster LONGTEXT NOT NULL,
+    summary LONGTEXT NOT NULL
 );
 
 
--- ================ user 회원
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-  id INT NOT NULL PRIMARY KEY,
-  password VARCHAR(20) NOT NULL,
-  email VARCHAR(40) NOT NULL,
-  name VARCHAR(20) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  sub INTEGER NOT NULL CHECK (sub IN (0, 1)), --구독유무
-  bookmark INT --구독기간
+-- ================ user 회원정보
+DROP TABLE IF EXISTS 회원정보;
+CREATE TABLE 회원정보 (
+  idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT UNIQUE,
+  pw VARCHAR(200) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  subscribe TINYINT NOT NULL CHECK (sub IN (0, 1)),
+  like_idx INT NOT NULL
 );
 
 -- ================ sub 구독
-DROP TABLE IF EXISTS sub;
-create table sub(
-    subinfo boolean not null, --구독결제유무
-    subdate date not null --구독기간
+DROP TABLE IF EXISTS 구독;
+create table 구독(
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    confirm TINYINT NOT NULL CHECK (sub IN (0, 1))
     );
 
 
 -- ================ bookmark 즐겨찾기
-DROP TABLE IF EXISTS bookmark;
-create table bookmark(
-    bookname varchar(100) not null, --제목
-    poster varchar not null --표지사진
+DROP TABLE IF EXISTS 즐겨찾기;
+create table 즐겨찾기(
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_idx INT NOT NULL,
+    book_idx INT NOT NULL
 );
 
--- ================ feed 피드
-DROP TABLE IF EXISTS feed;
-create table feed(
-    feedname varchar(100) not null, --제목
-    thumbnail varchar not null,--썸네일
-    content varchar(500) not null--내용
+-- ================ feed 피드 랜덤으로 view에 띄워주기 "좋아요"
+DROP TABLE IF EXISTS 좋아요;
+create table 좋아요(
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_idx INT NOT NULL,
+    feed_idx int NOT NULL
 );
+
+-- ================ feed
+DROP TABLE IF EXISTS 피드;
+create table 피드(
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    writer varchar(100) NOT NULL,
+    title varchar(200) NOT NULL ,
+    content LONGTEXT NOT NULL,
+    create_date DATE NOT NULL,
+    views INT NOT NULL,
+    likes INT NOT NULL,
+    bookinfo_idx INT NOT NULL
+);
+-- ================ 리뷰
+DROP TABLE IF EXISTS 리뷰;
+create table 리뷰(
+    idx INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    feed_idx INT NOT NULL,
+    writer_idx INT NOT NULL,
+    content LONGTEXT NOT NULL,
+    create_date DATE NOT NULL,
+    bookinfo_idx INT NOT NULL
+    );
+
+
